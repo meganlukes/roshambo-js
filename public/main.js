344d0f4ff97ca76c9a4930ba3b2f5e8e3445b247
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-05-21 11:29:40 -0400

    Changed classes

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let turn = 0
+//Let player 1 choose and assign picture to their list item
+function chooseRock() {
+  var newPic = document.getElementsByTagName('img').rock
+  if (turn % 2 === 0) {
+    var playerImg = 
+  } else {
+    const player2picture = document.querySelector('.player2 image')
   }
+  turn++
+}
+function choosePaper() {
+  if (turn % 2 === 0) {
+    const player1Picture = document.querySelector('.player1 img')
+  } else {
+    const player2picture = document.querySelector('.player2 img')
+  }
+}
+function chooseScissors() {
+  if (turn % 2 === 0) {
+    const player1Picture = document.querySelector('.player1 img')
+  } else {
+    const player2picture = document.querySelector('.player2 img')
+  }
+}
+//Formula to determine who won using codewars example
+
+function main() {
+  document.querySelector('li.rock').addEventListener('click', chooseRock)
 }
 
 document.addEventListener('DOMContentLoaded', main)
