21e144d608b7abc670464d1268fb1f7efae93f84
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-05-21 14:39:47 -0400

    Added choose paper and choose scissors

--- public/main.js
+++ public/main.js
@@ -1,32 +1,79 @@
 let turn = 0
-//Let player 1 choose and assign picture to their list item
+
 function chooseRock() {
-  var newPic = document.getElementsByTagName('img').rock
   if (turn % 2 === 0) {
-    var playerImg = 
+    document
+      .querySelector('.player-1-img')
+      .setAttribute('src', './pics/Rock.jpg')
   } else {
-    const player2picture = document.querySelector('.player2 image')
+    document
+      .querySelector('.player-2-img')
+      .setAttribute('src', './pics/Rock.jpg')
   }
   turn++
 }
 function choosePaper() {
   if (turn % 2 === 0) {
-    const player1Picture = document.querySelector('.player1 img')
+    document
+      .querySelector('.player-1-img')
+      .setAttribute('src', './pics/Paper.jpg')
   } else {
-    const player2picture = document.querySelector('.player2 img')
+    document
+      .querySelector('.player-2-img')
+      .setAttribute('src', './pics/Paper.jpg')
   }
+  turn++
 }
 function chooseScissors() {
   if (turn % 2 === 0) {
-    const player1Picture = document.querySelector('.player1 img')
+    document
+      .querySelector('.player-1-img')
+      .setAttribute('src', './pics/Scissors.jpg')
   } else {
-    const player2picture = document.querySelector('.player2 img')
+    document
+      .querySelector('.player-2-img')
+      .setAttribute('src', './pics/Scissors.jpg')
+  }
+  turn++
+}
+
+function whoWon() {
+  const player1Choice = document.querySelector('.player-1-img')
+  const player2Choice = document.querySelector('.player-2-img')
+  const winner = document.querySelector('h2')
+
+  if (player2Choice.includes('placeholder')) {
+    return
+  } else if (player1Choice === player2Choice) {
+    winner.textContent = 'Tie!'
+  } else if (
+    player1Choice.includes('rock') &&
+    player2Choice.includes('scissors')
+  ) {
+    winner.textContent = 'Player 1 Wins!'
+  } else if (
+    player1Choice.includes('paper') &&
+    player2Choice.includes('rock')
+  ) {
+    winner.textContent = 'Player 1 Wins!'
+  } else if (
+    player1Choice.includes('scissors') &&
+    player2Choice.includes('paper')
+  ) {
+    winner.textContent = 'Player 1 Wins!'
+  } else {
+    winner.textContent = 'Player 2 Wins!'
   }
 }
-//Formula to determine who won using codewars example
 
 function main() {
-  document.querySelector('li.rock').addEventListener('click', chooseRock)
+  document.querySelector('li #rock').addEventListener('click', chooseRock)
+
+  document.querySelector('li #paper').addEventListener('click', choosePaper)
+
+  document
+    .querySelector('li #scissors')
+    .addEventListener('click', chooseScissors)
 }
 
 document.addEventListener('DOMContentLoaded', main)
